e59cded750ded4f43a5245b3437659edea342c69
MarinaBartulov <bartulovmarina1@gmail.com>
2022-01-11 19:58:47 +0100

    initial truffle and react projects created

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import logo from '../assets/logo.png';
+import './App.css';
+
+import web3 from 'web3';
+
+function App() {
+  return (
+    <div className="App">
+        Hello world!
+    </div>
+  );
+}
+
+export default App;
